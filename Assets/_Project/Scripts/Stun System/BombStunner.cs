309ccf1173dba030a53fa12581e548ec045f04ce
William <william.plank@outlook.fr>
2025-12-15 12:33:18 +0100

    polish freezer

--- Assets/_Project/Scripts/Stun System/BombStunner.cs
+++ Assets/_Project/Scripts/Stun System/BombStunner.cs
@@ -1,9 +1,15 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 public class BombStunner : BaseStunner
 {
+    public UnityEvent<Vector3> OnPaintFreezerDie;
     private void OnEnable() => PaintBlob.OnAnyPaintFreezerDie += PaintBlob_OnAnyPaintFreezerDie;
     private void OnDisable() => PaintBlob.OnAnyPaintFreezerDie -= PaintBlob_OnAnyPaintFreezerDie;
 
-    private void PaintBlob_OnAnyPaintFreezerDie(Vector3 deathWorldPosition) => TryStun(deathWorldPosition);
+    private void PaintBlob_OnAnyPaintFreezerDie(Vector3 deathWorldPosition)
+    {
+        OnPaintFreezerDie?.Invoke(deathWorldPosition);
+        TryStun(deathWorldPosition);
+    }
 }
