355db987a0b95f4a178fded0ae85a4a2954b0303
William <william.plank@outlook.fr>
2025-12-30 14:35:10 +0100

    add scene transition

--- Assets/_Project/Scripts/Utility/SceneLoader.cs
+++ Assets/_Project/Scripts/Utility/SceneLoader.cs
@@ -5,18 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : PersistentMonoSingleton<SceneLoader>
 {
+    [SerializeField] private Animator transitionAnimator;
+    [SerializeField, Range(0f,3f)] private float transitionDuration = 1f;
+
+    private readonly int END = Animator.StringToHash("end");
+    private readonly int START = Animator.StringToHash("start");
+
     public void LoadSceneAsync(int buildIndex) => StartCoroutine(LoadSceneAsyncCoroutine(buildIndex));
     public void LoadNextSceneAsync() => StartCoroutine(LoadSceneAsyncCoroutine((SceneManager.GetActiveScene().buildIndex + 1) % 
         SceneManager.sceneCountInBuildSettings));
 
-    public void ReloadSceneAsync() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);// StartCoroutine(LoadSceneAsyncCoroutine(SceneManager.GetActiveScene().buildIndex));
+    public void ReloadSceneAsync() => StartCoroutine(LoadSceneAsyncCoroutine(SceneManager.GetActiveScene().buildIndex));
     private IEnumerator LoadSceneAsyncCoroutine(int buildIndex)
     {
+        transitionAnimator.SetTrigger(END);
+        yield return new WaitForSecondsRealtime(transitionDuration);
+
         AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(buildIndex);
+
+        transitionAnimator.SetTrigger(START);
 
         while (!asyncLoad.isDone)
         {
             yield return null;
         }
+
+      
     }
 }
