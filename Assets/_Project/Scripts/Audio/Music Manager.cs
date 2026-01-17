908f515acdf4bd8aa967ba7f58c7abdbc6c2ff3b
William <william.plank@outlook.fr>
2026-01-17 15:59:18 +0100

    add music for boss

--- Assets/_Project/Scripts/Audio/Music Manager.cs
+++ Assets/_Project/Scripts/Audio/Music Manager.cs
@@ -11,13 +11,19 @@ public class MusicManager : BaseMusicManager
     [SerializeField] private AudioClip upgradeMusic;
     [SerializeField, Range(0f,2f)] private float upgradeMusicCrossfadeDuration = 1f;
 
+    [SerializeField] private AudioClip bossMusic, bossUpgradeMusic;
+
     private void OnEnable() => GameManager.OnStartGameState += GameManager_OnStartGameState;
     private void OnDisable() => GameManager.OnStartGameState -= GameManager_OnStartGameState;
 
     protected override void Awake()
     {
         base.Awake();
-        PlayMusicAtIndex(0);
+
+        if (!LevelManager.Instance.CurrentLevelData.IsBossLevel)
+            PlayMusicAtIndex(0);
+        else
+            PlayMusicAtIndex(1);
     }
 
     private void GameManager_OnStartGameState(GameManager.GameState gamestate)
@@ -25,10 +31,16 @@ public class MusicManager : BaseMusicManager
         switch (gamestate)
         {
             case GameManager.GameState.PAINT:
-                CrossfadeToClip(paintMusic, paintMusicCrossfadeDuration, true);
+                if(LevelManager.Instance.CurrentLevelData.IsBossLevel)
+                    CrossfadeToClip(bossMusic, paintMusicCrossfadeDuration, true);
+                else
+                    CrossfadeToClip(paintMusic, paintMusicCrossfadeDuration, true);
                 break;
             case GameManager.GameState.DAY_SUMMARY:
-                CrossfadeToClip(upgradeMusic, upgradeMusicCrossfadeDuration, true);
+                if (LevelManager.Instance.CurrentLevelData.IsBossLevel)
+                    CrossfadeToClip(bossUpgradeMusic, upgradeMusicCrossfadeDuration, true);
+                else
+                    CrossfadeToClip(upgradeMusic, upgradeMusicCrossfadeDuration, true);
                 break;
             case GameManager.GameState.UPGRADE:
                 break;
