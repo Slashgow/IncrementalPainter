6c3a1dc05723028d28b243dd41f86e48dcbff258
William <william.plank@outlook.fr>
2026-03-03 14:54:40 +0100

    eater ennemy

--- Assets/_Project/Scripts/Boss/The Cleaner/EaterEnemy.cs
+++ Assets/_Project/Scripts/Boss/The Cleaner/EaterEnemy.cs
@@ -1,5 +1,20 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class EaterEnemy : MonoBehaviour
 {
+    public static event Action<Vector3> OnEatAnyBlob;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.TryGetComponent(out PaintBlob blob))
+        {
+            IDamageable damageable = blob.GetComponentInParent<IDamageable>();
+            if(damageable != null)
+            {
+                damageable.Die();
+                OnEatAnyBlob?.Invoke(transform.position);
+            }
+        }
+    }
 }
---
+++ Assets/_Project/Scripts/Boss/The Cleaner/EaterManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class EaterManager : MonoBehaviour
+{
+    [SerializeField] private UnityEvent<Vector3> onEatBlob;
+
+    private void OnEnable() => EaterEnemy.OnEatAnyBlob += HandleEatAnyBlob;
+    private void OnDisable() => EaterEnemy.OnEatAnyBlob -= HandleEatAnyBlob;
+    private void HandleEatAnyBlob(Vector3 vector) => onEatBlob.Invoke(vector);
+}
